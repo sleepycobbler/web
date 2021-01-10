912defbfd503236d68449422c1271faef4e83f4d
sleepycobbler <sleepycobbler@gmail.com>
2021-01-09 22:22:49 -0700

    added good looking stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week 1 notes",
       url: "week1/index.html"
+    },
+    {
+        label: "Week 2 notes",
+        url: "week2/index.html"
+    },
+    {
+        label: "Week 3 notes",
+        url: "week3/index.html"
+    },
+    {
+        label: "Week 4 notes",
+        url: "week4/index.html"
+    },
+    {
+        label: "Week 5 notes",
+        url: "week5/index.html"
+    },
+    {
+        label: "Week 6 notes",
+        url: "week6/index.html"
+    },
+    {
+        label: "Week 7 notes",
+        url: "week7/index.html"
+    },
+    {
+        label: "Week 8 notes",
+        url: "week8/index.html"
+    },
+    {
+        label: "Week 9 notes",
+        url: "week9/index.html"
+    },
+    {
+        label: "Week 10 notes",
+        url: "week10/index.html"
+    },
+    {
+        label: "Week 11 notes",
+        url: "week11/index.html"
+    },
+    {
+        label: "Week 12 notes",
+        url: "week12/index.html"
+    },
+    {
+        label: "Week 13 notes",
+        url: "week13/index.html"
     }
   ]
 
